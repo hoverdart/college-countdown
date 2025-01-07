953d49625bd0c3026d9b08bfcaae251bb9f0df22
Syntax <66713007+hoverdart@users.noreply.github.com>
2025-01-07 01:27:34 +0000

    making sure you can see the title

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
   return (
     <div className="items-center min-h-screen p-8 pb-8 gap-8 sm:p-8 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">College Countdown</h1>
+      <h1 className="text-2xl/7 font-bold text-gray-900 dark:text-white sm:truncate sm:text-3xl sm:tracking-tight">College Countdown</h1>
       <RandomQuote />
       <CollegeTable />
 
