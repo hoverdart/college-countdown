41773fd2fe099dea515b2663215419ad84261bcd
Syntax <66713007+hoverdart@users.noreply.github.com>
2025-01-14 16:10:44 -0800

    Update EachCountdown.tsx - i very much dislike typescript and their "vArIaBlE nEvEr uSeD"

--- app/components/EachCountdown.tsx
+++ app/components/EachCountdown.tsx
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react';
 const CountdownTimer = ({ tilThisDate }: { tilThisDate: string }) => {
   const dater = new Date(tilThisDate);
   const calculateTimeLeft = () => {
-    const easternTime = new Date().toLocaleString("en-US", {timeZone: 'America/New_York'});
-    const easternTimeInMS = Date.parse(easternTime)
+    //const easternTime = new Date().toLocaleString("en-US", {timeZone: 'America/New_York'});
+    //const easternTimeInMS = Date.parse(easternTime) //why am i commenting this out? because typescript. That's why.
     const time = dater.getTime() - date.now(); //not easternTimeInMS
     let timeLeft: {
       Months?: number;
